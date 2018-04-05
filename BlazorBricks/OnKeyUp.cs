0575739ccb568d714716a50139232990aaff5591
marcelooliveira <mclricardo@gmail.com>
2018-04-05 00:57:04 -0300

    e agora?

---
+++ BlazorBricks/OnKeyUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBricks
+{
+    public static class OnKeyUp
+    {
+        public static Action<string> Action { get; set; }
+        public static void Handler(string value)
+        {
+            Action?.Invoke(value);
+        }
+    }
+}
