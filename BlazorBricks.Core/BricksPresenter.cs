65570a88249e77b3c8bcdb4745ff594814f92f31
marcelooliveira <mclricardo@gmail.com>
2018-04-15 16:56:27 -0300

    .

--- BlazorBricks.Core/BricksPresenter.cs
+++ BlazorBricks.Core/BricksPresenter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using BlazorBricks.Core.Shapes;
 
@@ -13,9 +14,12 @@ namespace BlazorBricks.Core
         private IView view;
         private BricksBoard BricksBoard;
         private TimeSpan accumulatedTimeSpan = TimeSpan.FromMilliseconds(0);
-        private const int TICK_MS_INTERVAL = 25;
-        private const int PROCESS_NEXT_MOVEMENT_MS_INTERVAL = 300;
-        
+        private CancellationTokenSource cancellationTokenSource;
+        private const int TICK_MS_INTERVAL = 10;
+        private const int PROCESS_NEXT_MOVEMENT_MS_INTERVAL = 150;
+
+        public bool IsGameOver { get; private set; } = true;
+
         public BricksPresenter(IView view)
         {
             this.view = view;
@@ -25,26 +29,31 @@ namespace BlazorBricks.Core
                 Updated?.Invoke(this, e);
             };
 
-            TickLoop();
+            StartTickLoop();
         }
 
-        private async Task TickLoop()
+        public async Task StartTickLoop()
         {
-            await Task.Run(async () =>
-            {
-                while (true)
-                {
-                    await Task.Delay(TICK_MS_INTERVAL);
-                    bool processMove = BricksBoard.DownPressed;
-                    accumulatedTimeSpan = accumulatedTimeSpan.Add(TimeSpan.FromMilliseconds(TICK_MS_INTERVAL));
-                    if (accumulatedTimeSpan.TotalMilliseconds >= PROCESS_NEXT_MOVEMENT_MS_INTERVAL)
-                    {
-                        processMove = true;
-                        accumulatedTimeSpan = TimeSpan.FromMilliseconds(0);
+            IsGameOver = false;
+            cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+            Task task = Task.Run(async () =>
+               {
+                   while (!IsGameOver)
+                   {
+                       await Task.Delay(TICK_MS_INTERVAL);
+                       bool processMove = BricksBoard.DownPressed;
+                       accumulatedTimeSpan = accumulatedTimeSpan.Add(TimeSpan.FromMilliseconds(TICK_MS_INTERVAL));
+                       if (accumulatedTimeSpan.TotalMilliseconds >= PROCESS_NEXT_MOVEMENT_MS_INTERVAL)
+                       {
+                           processMove = true;
+                           accumulatedTimeSpan = TimeSpan.FromMilliseconds(0);
+                       }
+                       Tick(processMove);
                     }
-                    Tick(processMove);
-                }
-            });
+               }, token);
+
+            await task;
         }
 
         public IView View
@@ -79,36 +88,45 @@ namespace BlazorBricks.Core
 
         public bool MoveLeft()
         {
+            if (IsGameOver) return false;
             return BricksBoard.MoveLeft();
         }
 
         public bool MoveRight()
         {
+            if (IsGameOver) return false;
             return BricksBoard.MoveRight();
         }
         
         public bool MoveDown()
         {
+            if (IsGameOver) return false;
             return BricksBoard.MoveDown();
         }
 
         public bool Rotate90()
         {
+            if (IsGameOver) return false;
             return BricksBoard.Rotate90();
         }
 
         public bool Rotate270()
         {
+            if (IsGameOver) return false;
             return BricksBoard.Rotate270();
         }
 
         public void InitializeBoard()
         {
+            view.Reset();
             BricksBoard.InitializeArray();
+            IsGameOver = false;
         }
 
         public void GameOver()
         {
+            cancellationTokenSource.Cancel();
+            //IsGameOver = true;
             view.GameOver();
         }
 
@@ -135,12 +153,6 @@ namespace BlazorBricks.Core
         {
             get { return BricksBoard.Level; }
         }
-
-        public bool IsPlaying
-        {
-            get { return BricksBoard.IsPlaying; }
-        }
-
     }
 
     public enum ShapeCodes
